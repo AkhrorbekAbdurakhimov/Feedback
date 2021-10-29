a5e0e084786fa4284573a200cf7d9bd5b6a899ae
Ilhomjon Ahadov <ilhomdev7@gmail.com>
2021-10-29 14:29:10 +0500

    Implementing get user info feature

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const Markup = require('telegraf/markup')
 const Extra = require('telegraf/extra')
 const session = require('telegraf/session')
 const Stage = require('telegraf/stage')
+const WizardScene = require('telegraf/scenes/wizard')
 const Scene = require('telegraf/scenes/base')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
@@ -14,50 +15,77 @@ bot.use(session())
 
 const { enter, leave } = Stage
 
-// Name Scene
-const nameScene = new Scene('name');
-nameScene.enter((ctx) => ctx.reply('Ism Familiyangizni kiriting.\n\nNamuna: Behruz Sarvarov'))
-nameScene.on('text', (ctx) => {
-    ctx.state.id = ctx.message.from.id;
-    ctx.state.name = ctx.message.text;
+// User Info Scene
 
+const userInfoWizard = new WizardScene('user-info', 
+  (ctx) => {
+    ctx.reply('Ism Familiyangizni kiriting.\n\nNamuna: Behruz Sarvarov');
+    ctx.wizard.state.userInfo = {};
+    ctx.wizard.state.userInfo.id = ctx.message.from.id;
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    // validation example
+    if (ctx.message.text.length < 4) {
+      ctx.reply('Iltimos, Ism Familiyangizni tog`ri kiriting');
+      return; 
+    }
+    ctx.wizard.state.userInfo.name = ctx.message.text;
     ctx.reply('Telefon raqamingiz', Extra.markup((markup) => {
-        return markup.resize()
-          .keyboard([
-            markup.contactRequestButton('Telefon raqam yuborish'),
-            
-        ])
-      
-    }))
-
-    //console.log(ctx.message)
+      return markup.resize()
+        .keyboard([
+          markup.contactRequestButton('Telefon raqam yuborish'),
+          
+      ])
     
-    console.log(ctx.state);
-    //ctx.state.contact = ctx.message.contact;
-})
+    }))
+    
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    ctx.wizard.state.userInfo.contact = ctx.message.contact.phone_number;
+    ctx.wizard.state.userInfo.created_at = ctx.message.date;
+    ctx.wizard.state.userInfo.bot_id = ctx.message.reply_
+    ctx.reply(ctx.message)
+  }
 
+
+)
+
+
+
+
+
+// const nameScene = new Scene('name');
+// nameScene.enter((ctx) => ctx.reply('Ism Familiyangizni kiriting.\n\nNamuna: Behruz Sarvarov'))
+// nameScene.on('text', (ctx) => {
+//     ctx.state.id = ctx.message.from.id;
+//     ctx.state.name = ctx.message.text;
+
+    // ctx.reply('Telefon raqamingiz', Extra.markup((markup) => {
+    //     return markup.resize()
+    //       .keyboard([
+    //         markup.contactRequestButton('Telefon raqam yuborish'),
+            
+    //     ])
+      
+    // }))
+    
+//     console.log(ctx.state);
+// })
   
-bot.on('contact', (ctx) => {
-    ctx.state.contact = ctx.message.contact.phone_number;
-    console.log(ctx.message);
-    console.log(ctx.state);
-})
+// nameScene.on('contact', (ctx) => {
+//     ctx.state.contact = ctx.message.contact.phone_number;
+//     //console.log(ctx.message);
+//     console.log(ctx.state);
+// })
 
 
 //nameScene.leave((ctx) => ctx.scene.enter('phone'));
 
-// Phone Number Scene
-const phoneScene = new Scene('phone');
-phoneScene.enter((ctx) => ctx.reply('Telefon raqamingiz', Extra.markup((markup) => {
-    return markup.resize()
-      .keyboard([
-        markup.contactRequestButton('Send contact'),
-        
-      ])
-  })
-))
 
-const stage = new Stage([nameScene, phoneScene], { ttl: 10 })
+
+const stage = new Stage([userInfoWizard], { ttl: 10 })
 bot.use(stage.middleware())
 
 bot.start((ctx) => {
@@ -69,14 +97,7 @@ bot.start((ctx) => {
       })
     )
 
-    ctx.scene.enter('name');
+    ctx.scene.enter('user-info');
 })
-
-
-
-
-
-
-
 
 bot.launch();
