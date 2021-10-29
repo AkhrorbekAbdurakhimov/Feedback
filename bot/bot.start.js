46e091106560064a75f82d87d1054e9761b9cf0c
AkhrorbekAbdurakhimov <akhrorbekabdurakhimov@gmail.com>
2021-10-29 19:24:31 +0500

    minor chnages

--- bot/bot.start.js
+++ bot/bot.start.js
@@ -1,12 +1,16 @@
-const fs = require('fs')
 const path = require('path');
-const axios = require('axios');
 const Telegraf = require('telegraf');
 const session = require("telegraf/session");
 const Stage = require('telegraf/stage');
 const WizardScene = require('telegraf/scenes/wizard');
 const TelegrafI18n = require('telegraf-i18n');
 const Bot = require('./../database');
+const { Markup } = Telegraf
+
+const inlineMessageRatingKeyboard = Markup.inlineKeyboard([
+    Markup.callbackButton("🇺🇿 O`zbekcha", 'uzbek'),
+    Markup.callbackButton('🇷🇺 Русский', 'russian')
+]).extra()
 
 const i18n = new TelegrafI18n({
     directory: path.resolve(__dirname, 'locales'),
@@ -23,54 +27,25 @@ const botStart = async (token) => {
     const bot = new Telegraf(token)
     bot.use(session());
     bot.use(i18n.middleware());
-    bot.command('start', async (ctx) => {
-        const message = ctx.i18n.t('greeting', {
-            first_name: ctx.from.first_name
-        })
-        ctx.replyWithMarkdown(message);
-        const profile_photo = await ctx.telegram.getUserProfilePhotos(ctx.from.id)
-        const fileId = profile_photo.photos[0][2].file_id
-        ctx.telegram.getFileLink(fileId).then(url => {    
-            axios({url, responseType: 'stream'}).then(response => {
-                return new Promise((resolve, reject) => {
-                    response.data.pipe(fs.createWriteStream(`${process.cwd()}/profiles/${ctx.from.id}.jpg`))
-                            .on('finish', async () => {
-                                await Bot.insertUser([
-                                    ctx.from.id,
-                                    ctx.from.first_name,
-                                    ctx.from.username,
-                                    `/profiles/${ctx.from.id}.jpg`,
-                                    ctx.chat.id
-                                ])
-                            })
-                            .on('error', e => console.log(e))
-                        });
-                    })
-        })
-    })
-    bot.command('uz', (ctx) => {
+    bot.start(ctx => ctx.reply('Tilni tanlang!', inlineMessageRatingKeyboard))
+    bot.action('uzbek', (ctx) => {
         ctx.i18n.locale('uz')
         const message = ctx.i18n.t('greeting', {
             first_name: ctx.from.first_name
         })
         ctx.replyWithMarkdown(message);
     })
-    bot.command('ru', (ctx) => {
+    bot.action('russian', (ctx) => {
         ctx.i18n.locale('ru')
         const message = ctx.i18n.t('greeting', {
             first_name: ctx.from.first_name
         })
         ctx.replyWithMarkdown(message);
     })
-    bot.command('help', (ctx) => {
-        ctx.reply(ctx.i18n.t('help'));
-    })
     const feedbackWizard = new WizardScene('feedback-wizard',
         (ctx) => {
             ctx.reply(ctx.i18n.t('output1'));
             ctx.scene.session.user = {}
-            ctx.scene.session.user.senderId = ctx.from.id
-            ctx.scene.session.user.recieverId = ctx.from.id
             return ctx.wizard.next();
         },
         (ctx) => {
@@ -89,13 +64,52 @@ const botStart = async (token) => {
             });
             return ctx.wizard.next();
         },
-        (ctx) => {
+        async (ctx) => {
             ctx.scene.session.user.phone_number = ctx.message.contact ? ctx.message.contact.phone_number : ctx.message.text;
             ctx.reply(ctx.i18n.t('output4'));
+            let currentUserId = await Bot.getUser(ctx.from.id)
+            if (!currentUserId.length) {
+                const profile_photo = await ctx.telegram.getUserProfilePhotos(ctx.from.id)
+                const fileId = profile_photo ? profile_photo.photos[0][2].file_id : ''
+                const file_url = await ctx.telegram.getFileLink(fileId)
+                await Bot.insertUser([
+                    ctx.from.id,
+                    ctx.from.first_name,
+                    ctx.from.username,
+                    ctx.scene.session.user.full_name,
+                    ctx.scene.session.user.phone_number,
+                    file_url,
+                    ctx.chat.id
+                ])
+            }
             return ctx.wizard.next();
         },
-        (ctx) => {
-            ctx.scene.session.user.message_id = ctx.message.message_id;
+        async (ctx) => {
+            let message = ctx.message.text, message_type = 'text';
+            if (ctx.message.document) {
+                message = await ctx.telegram.getFileLink(ctx.message.document.file_id)
+                message_type = ctx.message.document.mime_type
+            } 
+            if (ctx.message.photo) {
+                message = await ctx.telegram.getFileLink(ctx.message.photo[2].file_id)
+                message_type = 'photo'
+            }
+            if (ctx.message.voice) {
+                message = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+                message_type = ctx.message.voice.mime_type
+            }
+            if (ctx.message.video) {
+                message = await ctx.telegram.getFileLink(ctx.message.video.file_id)
+                message_type = ctx.message.video.mime_type
+            }
+            await Bot.insertMessage([
+                ctx.message.message_id,
+                ctx.from.id,
+                ctx.chat.id,
+                message_type,
+                message,
+                new Date(1000 * ctx.message.date),
+            ])
             ctx.reply(ctx.i18n.t('output5'));
             return ctx.scene.leave();
         }
--- database/index.js
+++ database/index.js
@@ -21,17 +21,41 @@ class Bot {
         return result.rows || []
     }
     
+    static async getUser (id) {
+        const sql = `
+            SELECT id FROM users WHERE id = $1;
+        `
+        const result = await database.query(sql, [id])
+        return result.rows || []
+    }
+    
     static async insertUser (user_details) {
         const sql = `
             INSERT INTO users (
                 id,
                 first_name,
                 username,
+                full_name,
+                phone_number,
                 profile_picture,
                 bot_id
-            ) values ($1, $2, $3, $4, $5)
+            ) values ($1, $2, $3, $4, $5, $6, $7)
         `
         const result = await database.query(sql, user_details);
+        return result.rows || []
+    }
+    static async insertMessage (message_details) {
+        const sql = `
+            INSERT INTO messages (
+                message_id,
+                sender_id,
+                reciever_id,
+                message_type,
+                message,
+                message_send_at
+            ) values ($1, $2, $3, $4, $5, $6)
+        `
+        const result = await database.query(sql, message_details);
         return result.rows || []
     }
 }
